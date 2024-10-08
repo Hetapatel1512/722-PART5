9fc24867f66eb79ab611982a7c4fc9e581f74cad
Hetapatel1512 <hetamp20@gmail.com>
2024-10-09 04:38:03 +1100

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,5 +4,8 @@ set -u
 : "$CONTAINER_REGISTRY"
 : "$VERSION"
 
+# Enable verbose output
+set -x
+
 # Use kubectl to deploy the services
 envsubst < ./scripts/kubernetes/deployment.yaml | kubectl apply -f -
