9645b88cfa7b2d0776b851fb038e662e06daf830
Hetapatel1512 <hetamp20@gmail.com>
2024-10-10 00:57:21 +1100

    Update config.py

--- book_catalog/app/config.py
+++ book_catalog/app/config.py
@@ -1,3 +1,3 @@
 import os
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://user:password@host:port/dbname')
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://sit722part5_yypa_user:KJC3PbCNEX7HrKMJUXMcaa0DMxP8755A@dpg-cs2ip2e8ii6s739jaqn0-a.oregon-postgres.render.com/sit722part5_yypa')
